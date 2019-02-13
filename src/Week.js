3f35bc6d4f5933401eb478c58fba2dd73155e188
Shaital Desai <shaital.desai@sbcglobal.net>
2019-02-12 23:11:08 -0800

    Implemented calendar grid
    Added a new component Week.js for the grid

--- src/Week.js
+++ src/Week.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import './App.css';
 
-const Week = (props) => (
-  <div>
-  {props.weeksArr.map(week => (
-    <div className='row'>
-      <div className="one">{week[0]}</div>
-      <div className="two">{week[1]}</div>
-      <div className="three">{week[2]}</div>
-      <div className="four">{week[3]}</div>
-      <div className="five">{week[4]}</div>
-      <div className="six">{week[5]}</div>
-      <div className="seven">{week[6]}</div>
-    </div>
-  ))}
-  </div>
-)
+const Week = (props) => {
+  // let arrayOfWeeks = props.arrOfWeeks();
+  // console.log('WEEKS:', arrayOfWeeks);
+  return (
+    <div>
+      {props.arrOfWeeks.map(week => (
+        <div className="row">
+          <div className="one">{week[0]}</div>
+          <div className="two">{week[1]}</div>
+          <div className="three">{week[2]}</div>
+          <div className="four">{week[3]}</div>
+          <div className="five">{week[4]}</div>
+          <div className="six">{week[5]}</div>
+          <div className="seven">{week[6]}</div>
+        </div>
+      ))}
+  </div>)
+}
 
 export default Week;
